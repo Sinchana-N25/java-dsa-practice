5454922c06ef424e2617167a133d71c75c66ace5
Sinchana <sinchana2505@gmail.com>
2025-08-01 18:38:35 +0530

    factorial of a number using recursion

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    public static int fact(int x) {
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * fact(x - 1);
+        }
+    }
+    public static void main(String[] args) {
+        int number = 5; 
+        int result = fact(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
